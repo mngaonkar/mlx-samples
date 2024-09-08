0d4b835eb88d0a0f4395676d717ebc88263fcbef
Mahadev Gaonkar <mahadevgaonkar@Mahadevs-MacBook-Pro.local>
2024-09-08 03:05:26 -0700

    MLP code

---
+++ neural_network.py
@@ -0,0 +1,25 @@
+from typing import Any
+import mlx.core as mlx
+import mlx.nn as nn
+
+class NeuralNet(nn.Module):
+    def __init__(self, in_dims:int, out_dims:int):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(in_dims, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, out_dims)
+        )
+    def __call__(self, x):
+        for i, l in enumerate(self.layers):
+            x = mlx.maximum(x, 0) if i > 0 else x
+            x = l(x)
+        return x
+    
+net = NeuralNet(100, 1)
+print(net)
+
+params = net.parameters()
+print(params['layers']['layers'][0]['weight'].shape)
